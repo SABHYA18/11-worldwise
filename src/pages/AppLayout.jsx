a91f1c5f470fe41481b58c4ce1cb02c5098553bd
SABHYA18 <sbhmhjn@gmail.com>
2023-07-01 01:56:39 +0530

    Created a footer component and other things

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,10 @@
-import AppNav from "../components/AppNav";
-
+// import AppNav from "../components/AppNav";
+import SideBar from "../components/SideBar";
 
 function AppLayout() {
   return (
     <div>
-      <AppNav />
-      <p>App</p>
+      <SideBar />
     </div>
   );
 }
