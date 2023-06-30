c91b9c51fa083d4b64ebcd8e2c7c9abb442672ce
SABHYA18 <sbhmhjn@gmail.com>
2023-06-30 21:46:11 +0530

    Created and Deleted some files and wrote some code

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,16 @@
+import AppNav from "../components/AppNav";
+import PageNav from "../components/PageNav";
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <div>WorldWise</div>;
+  return (
+    <div>
+      <PageNav />
+      <AppNav />
+      <h1>WorldWise</h1>
+      <Link to="/app">Go to the App</Link>
+    </div>
+  );
 }
 
 export default HomePage;
