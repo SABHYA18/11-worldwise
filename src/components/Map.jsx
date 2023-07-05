8f40efca2f24b9f40227d2f38aac09a2f25a186d
SABHYA18 <sbhmhjn@gmail.com>
2023-07-06 02:37:27 +0530

    Fixed the adding, deleting and moving the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -12,19 +12,18 @@ import {
 import { useCities } from "../contexts/CitiesContext";
 import { useGeolocation } from "../hooks/useGeolocation";
 import Button from "./Button";
+import { useURLPosition } from "../hooks/useURLPosition";
 
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geoLocationPosition,
     getPosition,
   } = useGeolocation();
 
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useURLPosition();
 
   useEffect(
     function () {
